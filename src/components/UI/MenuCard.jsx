879d9164452908cba320fc93667c685cede944f8
soumikpatra1993 <tutul.smk@gmail.com>
2020-10-11 09:13:02 +0530

    search-bar-init

--- src/components/UI/MenuCard.jsx
+++ src/components/UI/MenuCard.jsx
@@ -1,12 +1,9 @@
-import React,{useState , useEffect}  from 'react'
+import React  from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import CardActions from '@material-ui/core/CardActions';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import IconButton from '@material-ui/core/IconButton';
 
 const MenuCard = ({dish:{strMeal,strMealThumb}}) => {
 
